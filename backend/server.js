f3482704039c047d3228c782722ce643f02236af
sebv <sebv@sv.home>
2020-10-08 17:24:47 +0200

    creation back end routes

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const { match } = require('assert')
+const express = require('express')
+const products = require('./data/products')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('API is running...')
+})
+
+// Get all products
+app.get('/api/products/', (req, res) => {
+  res.json(products)
+})
+
+// Get one product by ID
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
+})
+
+app.listen(5000, console.log('Server run on port 5000'))
