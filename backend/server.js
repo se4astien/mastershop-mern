41c32f7d54dd7ad51fb9fe93852fdd33ee1818a1
sebv <sebv@sv.home>
2020-10-09 11:06:29 +0200

    connecting to the database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 // const { match } = require('assert')
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
