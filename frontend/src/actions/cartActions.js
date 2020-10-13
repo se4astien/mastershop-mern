3ce50f05e83398faa23e28ebf45efab553032c34
sebv <sebv@sv.home>
2020-10-13 11:16:03 +0200

    add to cart functionnality

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,21 +1,21 @@
 import axios from 'axios'
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
+import { CART_ADD_ITEM } from '../constants/cartConstants'
 
-export const addToCart = (id, qty) => async (dispatch, getState) {
-    const {data} = await axios.get(`/api/products/${id}`)
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/products/${id}`)
 
-    dispatch({
-        type: CART_ADD_ITEM,
-        payload: {
-            product: data._id,
-            name: data.name,
-            image: data.image,
-            price: data.price,
-            countInStock: data.countInStock,
-            qty
-        }
-    })
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty,
+    },
+  })
 
-    // Save data into local storage if we want to be logged event after closing the website
-    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+  // Save data into local storage if we want to be logged event after closing the website
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
--- frontend/src/pages/CartScreen.js
+++ frontend/src/pages/CartScreen.js
@@ -1,6 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import Message from '../components/Message'
+import { addToCart } from '../actions/cartActions'
 
-const CartScreen = () => {
+// we need match to get the url,
+// location to get the quantity and a query string
+// history used to redirect
+const CartScreen = ({ match, location, history }) => {
+  // Getting our product ID from the URL
+  const productId = match.params.id
+
+  // Get the url with quantity => ?qty=1
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1
+  // console.log(qty)
+
+  const dispatch = useDispatch()
+
+  // The store is now initialized, we can also get those items and put them in our cart with useSelector
+  const cart = useSelector((state) => state.cart)
+  const { cartItems } = cart
+  console.log(cart)
+  console.log(cartItems)
+
+  // dispatch add to cart if there is a product id
+  // => IF we have an id and quantity, that is going to get added to our card
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty))
+    }
+  }, [dispatch, productId, qty])
+
   return (
     <div>
       <h1>cart</h1>
